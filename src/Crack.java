7c667a8c61ea4058f701b3ff80d576813146a161
Trevor Hartman <viable.hartman@gmail.com>
2022-10-26 00:00:37 -0700

    Initial commit of Java Lab 9

--- src/Crack.java
+++ src/Crack.java
@@ -20,23 +20,6 @@ public class Crack {
     }
 
     public void crack() throws FileNotFoundException {
-        InputStream is = new FileInputStream(this.dictionary);
-        try (Scanner sc = new Scanner(is, StandardCharsets.UTF_8)) {
-            while(sc.hasNextLine()) {
-                String word = sc.nextLine();
-                for(User user : users) {
-                    // We are only interested in cracking actual passwords, so let's ignore the "*"
-                    if( user.getPassHash().contains("$") ) {
-                        //System.out.printf("Testing password %s for user %s%n", word, user.getUsername());
-                        String hash = Crypt.crypt(word, user.getPassHash());
-                        //System.out.printf("Crypt Hash: %s%nUser Hash : %s%n", hash, user.getPassHash());
-                        if (hash.equals(user.getPassHash())) {
-                            System.out.printf("!!! Found password %s for user %s%n", word, user.getUsername());
-                        }
-                    }
-                }
-            }
-        }
     }
 
     public static int getLineCount(String path) {
@@ -48,19 +31,6 @@ public class Crack {
     }
 
     public static User[] parseShadow(String shadowFile) throws FileNotFoundException {
-        User[] users = new User[Crack.getLineCount(shadowFile)];
-
-        int index = 0;
-        InputStream is = new FileInputStream(shadowFile);
-        try (Scanner sc = new Scanner(is, StandardCharsets.UTF_8)) {
-            while(sc.hasNextLine()) {
-                String[] userLine = sc.nextLine().split(":");
-                User user = new User(userLine[0], userLine[1]);
-                users[index] = user;
-                index++;
-            }
-        }
-        return users;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
